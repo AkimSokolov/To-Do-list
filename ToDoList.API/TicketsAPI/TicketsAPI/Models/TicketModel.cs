cde9ac30a93360b0b7c909a2f1c8a988809a083e
badcodeenjoyer <somanyskillsinme@gmail.com>
2022-01-23 00:16:43 +0200

    Added base REST API

---
+++ ToDoList.API/TicketsAPI/TicketsAPI/Models/TicketModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TicketsAPI.Models;
+namespace TicketsAPI.Models
+{
+    public class TicketModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public List<SubtaskModel>? Subtasks { get; set; }
+        [Required]
+        public string Type { get; set; }
+        public int Status { get; set; }
+    }
+}
