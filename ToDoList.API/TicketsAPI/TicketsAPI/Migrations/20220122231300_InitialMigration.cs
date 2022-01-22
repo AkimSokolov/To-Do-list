25447785b527ca04a2e61cf78d7b13605e74e607
badcodeenjoyer <somanyskillsinme@gmail.com>
2022-01-23 01:34:06 +0200

    Published without Runtime

---
+++ ToDoList.API/TicketsAPI/TicketsAPI/Migrations/20220122231300_InitialMigration.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketsAPI.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TicketModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TicketModel", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubtaskModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    TicketModelId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubtaskModel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SubtaskModel_TicketModel_TicketModelId",
+                        column: x => x.TicketModelId,
+                        principalTable: "TicketModel",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubtaskModel_TicketModelId",
+                table: "SubtaskModel",
+                column: "TicketModelId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SubtaskModel");
+
+            migrationBuilder.DropTable(
+                name: "TicketModel");
+        }
+    }
+}
